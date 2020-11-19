18f92d8d859410287ef47ec67a447ea74cdad4f7
Joel Bryan Juliano <joel.juliano@dazn.com>
2020-11-19 10:33:18 +0100

    feat: add beamer to allowed org-mode exports

--- config/custom.el
+++ config/custom.el
@@ -1,6 +1,6 @@
 ;; custom emacs variables
 (custom-set-variables
- '(org-export-backends '(ascii html icalendar latex md odt))
+ '(org-export-backends '(ascii beamer html icalendar latex md odt))
  '(package-archives
    '(("melpa" . "http://melpa.org/packages/")
      ("gnu" . "https://elpa.gnu.org/packages/")))
