36bbe010f801f3784fbbb89ac2dce0470483caa9
Joel Bryan Juliano <joel.juliano@dazn.com>
2021-04-30 20:34:44 +0200

    feat: added TSX and JSX support

--- config/01-melpa.el
+++ config/01-melpa.el
@@ -166,4 +166,24 @@
        (setq company-tooltip-align-annotations t)
 
        (add-hook 'before-save-hook 'tide-format-before-save)
-       (add-hook 'typescript-mode-hook #'setup-tide-mode)))
+       (add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+       ;; for handling TSX and JSX files
+       (require 'web-mode)
+
+       (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+       (add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+       ;; enable typescript-tslint checker
+       (flycheck-add-mode 'typescript-tslint 'web-mode)
+
+       (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+       (add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "jsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+       ;; configure jsx-tide checker to run after your default jsx checker
+       (flycheck-add-mode 'javascript-eslint 'web-mode)
+       (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)))
